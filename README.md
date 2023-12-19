9b35e6d1ee74a1c948a459a04cd7971e3a3912c1
Adam Johnston <adam.avix.io@gmail.com>
2023-12-18 21:18:26 -0700

    uploading quiz

---
+++ README.md
@@ -0,0 +1,7 @@
+# Steps
+
+1. You need to initialize this project with npm and also create a tsconfig.json file for it to compile. You will need to globally install ts-node to run it.
+    - Before you start complaining, remember that React, Svelte, and everything else does this and you need to know it.
+2. Solve all of the problems in api.ts. They do not need to be perfect or the most efficient. Just get the right answer.
+3. Make sure you import the api.json file (you will need ts-node), and the first function returns the data. Try to console.log it.
+4. This is going to be the base of learning React.
---
+++ api.ts
@@ -0,0 +1,41 @@
+// You will need to import the api.json file here
+
+interface Data {
+    // Fill this in using ChatGPT from api.json for typescript
+}
+
+// Make sure you type what is returned from this function
+// Do not let it auto type for this exercise
+const fetchData = () => {
+    // Write a function that imports the api.json file and returns the data.
+}
+
+// Call functions like this to use them while developing your API
+fetchData()
+
+//
+//
+// For all questions below, you will have to call fetchData
+//
+//
+
+// Write a function that finds a user by an ID and returns the user
+// Return -1 if the user is not found (hint look for javascript functions that do this for you)
+
+// Write a function that finds a person by name (lowercase or uppercase) and returns the user
+
+// Write a function that gives a list of all deleted todos
+// You MUST use map here
+
+// Write a function that gives a list of all todos that are not done
+// You MUST use filter here
+
+// Write a function that returns all users that start with the name before "p" alphabetically
+
+// Write a function that returns all todos that are done AND deleted
+
+// Return a new list of todos that does not modify the original array, and marks them all as done (true).
+// Like this [{text: "text", done: false}, ...]
+
+// Modify the returned todos to only give a list with "text" and "done" fields
+// Like this [{text: "text", done: false}, ...]
